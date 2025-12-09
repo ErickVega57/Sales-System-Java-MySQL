b7ed21868b6393a63d574f10f3f938f65d6c7c7d
Lightz18 <a24216391@alumnos.uady.mx>
2025-12-08 20:21:20 -0600

    traduccuion pendiente

--- src/main/java/org/borghisales/salessysten/controllers/MainController.java
+++ src/main/java/org/borghisales/salessysten/controllers/MainController.java
@@ -11,7 +11,7 @@ import java.util.ResourceBundle;
 
 public class MainController extends MenuController implements Initializable {
     @FXML
-    private  TextField user;
+    private TextField user;
     @FXML
     private TextField password;
 
@@ -33,9 +33,10 @@ public class MainController extends MenuController implements Initializable {
     @FXML
     private void signIn(){
         if (SellerDAO.login(user.getText(),password.getText())) {
-            openNewStage(MANAGEMENT_VIEW_FXML, "Management");
+            openNewStage(MANAGEMENT_VIEW_FXML, "Gestion del punto de venta");
             closeCurrentStage(user);
         }
     }
+
 
 }
